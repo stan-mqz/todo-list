6f8989a733785ae7fd15ecb8a8dec8bd1a5b1140
Stan <moscarstanley37@gmail.com>
2024-10-14 21:02:58 -0600

    logic created

--- src/reducers/todoReducer.ts
+++ src/reducers/todoReducer.ts
@@ -80,9 +80,19 @@ export const todoReducer = (state = initialState, action: todoAction) => {
 
   if (action.type === 'filter-todo') {
 
+    let filteredTodo : TodoFormData[] = []
+
+    if (action.payload.filter === 1) {
+      filteredTodo = state.todoList
+    } else if(action.payload.filter === 2) {
+      filteredTodo = state.todoList.filter(todo => todo.status === 1)
+    } else {
+      filteredTodo = state.todoList.filter(todo => todo.status === 2)
+    }
 
     return {
       ...state,
+      todoList : [...filteredTodo],
       selectedFilter : action.payload.filter
     }
   }
