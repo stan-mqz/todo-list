f5fd300be8081248199d129e09c0f4de43570ff5
Stan <moscarstanley37@gmail.com>
2024-10-15 20:40:47 -0600

    Installed autoanimate package which includes animations for the Cards component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Header } from "./components/Header";
 import { TodoFormData } from "./types/types";
 import { todoReducer, initialState } from "./reducers/todoReducer";
 import { Cards } from "./components/Cards";
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 function App() {
   const formInitialState: TodoFormData = {
@@ -15,24 +16,25 @@ function App() {
   };
 
   const [todo, setTodo] = useState(formInitialState);
- 
   const [state, dispatch] = useReducer(todoReducer, initialState);
 
   const toggleState = (id: string) => {
-    dispatch({type: 'set-status', payload : {id}})
-  }
+    dispatch({ type: "set-status", payload: { id } });
+  };
 
+  const filteredTodo =
+    state.selectedFilter !== 0
+      ? state.todoList.filter((todo) => todo.status === state.selectedFilter)
+      : state.todoList;
 
-  const filteredTodo = state.selectedFilter !== 0 ? state.todoList.filter(todo => todo.status === state.selectedFilter) : state.todoList
-
+  const [parent] = useAutoAnimate();
 
   return (
     <>
       <Header />
 
-      <Combobox   
-      dispatch = {dispatch}
-      />
+      <Combobox dispatch={dispatch} />
+
 
       <Form
         todo={todo}
@@ -42,20 +44,22 @@ function App() {
         dispatch={dispatch}
       />
 
-      {filteredTodo.map((todo) => (
-        <div
-          key={todo.id}
-          className="bg-slate-100 p-2 mt-3 mb-3 max-w-[60%] mx-auto"
-        >
-          <Cards 
-          title={todo.title} 
-          checked={todo.status === 2}
-          toggleState={() => toggleState(todo.id)}
-          todo={todo}
-          dispatch = {dispatch}
-          />
-        </div>
-      ))}
+      <div ref={parent}>
+        {filteredTodo.map((todo) => (
+          <div
+            key={todo.id}
+            className="bg-slate-100 p-2 mt-3 mb-3 max-w-[60%] mx-auto"
+          >
+            <Cards
+              title={todo.title}
+              checked={todo.status === 2}
+              toggleState={() => toggleState(todo.id)}
+              todo={todo}
+              dispatch={dispatch}
+            />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -2,13 +2,12 @@ import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
 import { todoAction } from "../reducers/todoReducer";
 import { TodoFormData } from "../types/types";
 
-
 type CardsProps = {
   title: string;
-  checked : boolean
-  toggleState :  () => void
-  todo : TodoFormData
-  dispatch : React.Dispatch<todoAction>
+  checked: boolean;
+  toggleState: () => void;
+  todo: TodoFormData;
+  dispatch: React.Dispatch<todoAction>;
 };
 
 export const Cards = ({
@@ -16,38 +15,34 @@ export const Cards = ({
   checked,
   todo,
   toggleState,
-  dispatch
+  dispatch,
 }: CardsProps) => {
-
-
   const setTodoData = () => {
-    dispatch({ type: "set-activeId", payload: { id : todo.id } })
-  }
+    dispatch({ type: "set-activeId", payload: { id: todo.id } });
+  };
 
   const deleteTodo = () => {
-    dispatch({type : 'delete-todo', payload: {id: todo.id}})
-  }
+    dispatch({ type: "delete-todo", payload: { id: todo.id } });
+  };
 
-  
-
-  
   return (
     <div className="flex justify-between items-center bg-white p-5">
       <div className="flex gap-2">
-        <input type="checkbox" onChange={toggleState} checked={checked}  />
-        <p
-          className={`text-base font-bold ${
-            checked ? "line-through" : ""
-          }`}
-        >
+        <input type="checkbox" onChange={toggleState} checked={checked} />
+        <p className={`text-base font-bold ${checked ? "line-through" : ""}`}>
           {title}
         </p>
-      
       </div>
 
       <div className="flex gap-2">
-        <PencilSquareIcon onClick={setTodoData}  className="size-6 text-black cursor-pointer" />
-        <TrashIcon onClick={deleteTodo} className="size-6 text-black cursor-pointer" />
+        <PencilSquareIcon
+          onClick={setTodoData}
+          className="size-6 text-black cursor-pointer"
+        />
+        <TrashIcon
+          onClick={deleteTodo}
+          className="size-6 text-black cursor-pointer"
+        />
       </div>
     </div>
   );
